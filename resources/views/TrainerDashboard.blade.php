18ebe7d3739c30e60a4929f7572338e2705954d1
jouw naam <jordygerrits690@gmail.com>
2020-10-26 10:14:07 +0100

    Css login bezig en andere pagina's

---
+++ resources/views/TrainerDashboard.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<title>Dashboard Trainers</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+    body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<div class="w3-content" style="max-width:1400px">
+
+    <header class="w3-container w3-center w3-padding-32">
+        <h1><b>HCAS Trainer Dashboard</b></h1>
+        <p>Welcome<span class="w3-tag"> # Enter Name </span></p>
+    </header>
+
+    <div class="w3-row">
+
+        <div class="w3-col l8 s12">
+            <div class="w3-card-4 w3-margin w3-white">
+                <div class="w3-container">
+                    <h3><b>Zondag</b></h3>
+                    <h5>Groep, <span class="w3-opacity">H1</span></h5>
+                    <h5>Training 2</h5>
+                </div>
+
+                <div class="w3-container">
+                    <p>Doel:  <span class="w3-opacity">Opwarmen</span></p>
+                    <h6>60:00 Minuten</h6>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="w3-card-4 w3-margin w3-white">
+                <div class="w3-container">
+                    <h3><b>Maandag</b></h3>
+                    <h5>Groep, <span class="w3-opacity">D2</span></h5>
+                    <h5>Training 1 & 3</h5>
+                </div>
+
+                <div class="w3-container">
+                    <p>Doel:  <span class="w3-opacity">Verbeteren van teamspelen</span></p>
+                    <h6>45:00 Minuten</h6>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="w3-card-4 w3-margin w3-white">
+                <div class="w3-container">
+                    <h3><b>Woensdag</b></h3>
+                    <h5>Groep, <span class="w3-opacity">F5</span></h5>
+                    <h5>Training 13</h5>
+                </div>
+
+                <div class="w3-container">
+                    <p>Doel:  <span class="w3-opacity">Verbeteren van techniek</span></p>
+                    <h6>30:00 Minuten</h6>
+                </div>
+            </div>
+        </div>
+
+        <div class="w3-col l4">
+            <div class="w3-card w3-margin w3-margin-top">
+                <div class="w3-container w3-white">
+                    <h4><b>Oefeningen</b></h4>
+                </div>
+            </div>
+            <hr>
+            <div class="w3-card w3-margin w3-margin-top">
+                <div class="w3-container w3-white">
+                    <h4><b>Trainingen</b></h4>
+                </div>
+            </div>
+        </div>
+    </div><br>
+</div>
+
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <title>Document</title>
+    <title>Registreren</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 </head>
 <body>
 
