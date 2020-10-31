19392c5f160a4173e5feee8c7bb62301a21ab292
jouw naam <jordygerrits690@gmail.com>
2020-10-31 16:05:13 +0100

    Auteur Design

--- resources/views/Admin/AdminDashboard.blade.php
+++ resources/views/Admin/AdminDashboard.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="nl">
-<title>Admin Dashboard</title>
+<title>Auteur Dashboard</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -18,7 +18,7 @@
 <div class="w3-content" style="max-width:1400px">
 
     <header class="w3-container w3-center w3-padding-32">
-        <h1><b>Admin Paneel</b></h1>
+        <h1><b>Auteur Paneel</b></h1>
     </header>
 
     <div class="w3-row">
@@ -26,14 +26,14 @@
         <div class="w3-col l8 s12">
             <div class="w3-card-4 w3-margin w3-white">
                 <div class="w3-container">
-                    <h3><b>Bestaande Accounts</b></h3>
+                    <h3><b>Bestaande oefeningen</b></h3>
                 </div>
 
                 <div class="w3-container">
-                    <p>Jordy Gerrits</p>
-                    <p>Bas Lemmens</p>
-                    <p>Koole Boy</p>
-                    <p>Crazy Bob</p>
+                    <p>Oefening #1</p>
+                    <p>Oefening #2</p>
+                    <p>Oefening #3</p>
+                    <p>Oefening #4</p>
                 </div>
             </div>
         </div>
@@ -41,16 +41,9 @@
         <div class="w3-col l4">
             <div class="w3-card w3-margin w3-margin-top">
                 <div class="w3-container w3-white">
-                    <h4><a href="/userDashboard">Account Toevoegen</a></h4>
+                    <h4><a href="/userDashboard">Nieuwe Oefening Toevoegen</a></h4>
                 </div>
-                <br>
-                <div class="w3-container w3-white">
-                    <h4><a href="/userDashboard">Oefening Maken</a></h4>
-                </div>
-                <br>
-                <div class="w3-container w3-white">
-                    <h4><a href="/userDashboard">Training Maken</a></h4>
-                </div>
+                
             </div>
         </div>
     </div><br>
