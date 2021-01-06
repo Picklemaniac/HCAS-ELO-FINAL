10ed69a18f557cf10924895acf3c555160a733ad
jouw naam <jordygerrits690@gmail.com>
2021-01-06 12:06:43 +0100

    Wachtwoord Veranderen pagina aangemaakt en css geschreven

---
+++ resources/views/auth/passwordconfirm.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Wachtwoord Veranderen</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link href="{{ asset('css/logincss/login.css') }}" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="container">
+        <div class="card card-container">
+            <div class="col-md-8">
+                <div class="card-header input">
+                    <img style="width: 250px;" src="https://lh3.googleusercontent.com/-2wRKG2ArTPY-TDH57fOrJeU_KVsm368dyJEyp2uddqaHU9bA-1t04wUsFns7JR6aHgZbQ=s170"/> Inloggen
+                </div>
+
+                <div class="card-body">
+                    <form method="POST">
+                        <div class="form-group row">
+                            <div class="col-md-6 input">
+                                <input placeholder="Wachtwoord" id="username" type="username">
+                            </div>
+                        </div>
+                        <div class="col-md-8 offset-md-4 center">
+                            <button type="submit" class="btn btn-primary">
+                            </button>
+                        </div>
+                    </form>
+                </div>
+        </div>
+    </div>
+</div>
+</body>
